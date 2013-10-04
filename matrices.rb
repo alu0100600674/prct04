b9bc97a206470991c7f05c03ba60d44bc58c8317
Juan Tareq Gonzalez de Chavez Perez <alu0100603866@ull.edu.es>
2013-10-04 13:53:00 +0100

    Matrices definidas

--- matrices.rb
+++ matrices.rb
@@ -1,2 +1,20 @@
 #! /usr/local/rvm/rubies/ruby-2.0.0-p247/bin/ruby
 
+#definimos 2 matrices por defecto
+a = [[3,5,6],[0,3,2],[5,5,4]] #matriz 1
+b = [[1,1,1],[2,3,4],[6,5,9]] #matriz 2
+
+c = Array.new		#matriz c, contendra el resultado de la suma
+d = Array.new		#matriz d, contendra el resultado de la multiplicacion
+
+m,n = a.size,a[1].size
+
+for i in 0...m do
+	c[i] = Array.new
+	d[i] = Array.new
+	for j in 0...n do
+		c[i][j] = 0
+		d[i][j] = 0
+	end
+end 		#definimos el tamaño de c y d y las incializamosa 0
+
