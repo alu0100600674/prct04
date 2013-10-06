a6b6954e9e84bb164ea33d766c961def59e999d3
SUAREZ ARMAS <alu4307@exthost2.(none)>
2013-10-06 22:56:25 +0100

    Multiplicación de matrices

--- matrices.rb
+++ matrices.rb
@@ -1,5 +1,8 @@
 #! /usr/local/rvm/rubies/ruby-2.0.0-p247/bin/ruby
 
+#podemos usar la libreria matrix para la multiplicacion
+require 'matrix'
+
 #definimos 2 matrices por defecto
 a = [[3,5,6],[0,3,2],[5,5,4]] #matriz 1
 b = [[1,1,1],[2,3,4],[6,5,9]] #matriz 2
@@ -26,6 +29,16 @@ for i in 0...m do
 	end
 end
 
+#hacer la multiplicacion
+
+e = Matrix[[3,5,6],[0,3,2],[5,5,4]] * Matrix[[1,1,1],[2,3,4],[6,5,9]]
+
+for i in 0...m do
+	for j in 0...n do
+		d[i][j] = e.component(i, j)
+	end
+end
+
 #mostrar las matrices
 
 puts "\n La Matriz 1 es = "
@@ -42,3 +55,16 @@ puts "\n La suma de las matrices es = "
 c.each do |fila|
 	puts fila.join(" ")
 end 
+
+puts "\n La multiplicacion de las matrices es = "
+#muestra cada valor en una linea
+#e.each { |e| puts e }
+
+#muestra cada fila por separado
+#for i in 0...m do
+#	puts e.row(i)
+#end
+
+d.each do |fila|
+	puts fila.join(" ")
+end
