0e14dc880d46dfcb6d4ac3b203457dfe9aef5937
SUAREZ ARMAS <alu4307@exthost2.(none)>
2013-10-07 19:40:23 +0100

    Multiplicación arreglada después de pedir datos

--- matrices.rb
+++ matrices.rb
@@ -1,8 +1,5 @@
 #! /usr/local/rvm/rubies/ruby-2.0.0-p247/bin/ruby
 
-#podemos usar la libreria matrix para la multiplicacion
-require 'matrix'
-
 #definimos 2 matrices por defecto
 #a = [[3,5,6],[0,3,2],[5,5,4]] #matriz 1
 #b = [[1,1,1],[2,3,4],[6,5,9]] #matriz 2
@@ -57,17 +54,16 @@ for i in 0...m do
 	end
 end
 
-#la multiplicación da problemas 
-
-#hacer la multiplicacion
-#
-#e = Matrix[[3,5,6],[0,3,2],[5,5,4]] * Matrix[[1,1,1],[2,3,4],[6,5,9]]
-#
-#for i in 0...m do
-#	for j in 0...n do
-#		d[i][j] = e.component(i, j)
-#	end
-#end
+#Hemos arreglado la multiplicacion de matrices haciendola manualmente
+#en vez de hacer uso de la libreria Matrix
+for i in 0...m do
+	for j in 0...n do
+		d[i][j] = 0
+		for k in 0...n do
+			d[i][j] = d[i][j] + (a[i][k] * b[k][j])
+		end
+	end
+end
 
 #mostrar las matrices
 
