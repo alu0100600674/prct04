2762ae7536f8053af0a36f84f09e7054acb23b48
Juan Tareq Gonzalez <alu0100603866@ull.edu.es>
2013-10-07 12:50:18 +0100

    Cambio estructura para solicitar los datos en lugar de dejarlos fijos

--- matrices.rb
+++ matrices.rb
@@ -4,13 +4,41 @@
 require 'matrix'
 
 #definimos 2 matrices por defecto
-a = [[3,5,6],[0,3,2],[5,5,4]] #matriz 1
-b = [[1,1,1],[2,3,4],[6,5,9]] #matriz 2
+#a = [[3,5,6],[0,3,2],[5,5,4]] #matriz 1
+#b = [[1,1,1],[2,3,4],[6,5,9]] #matriz 2
+
+a = Array.new #matriz a pedir datos
+b = Array.new #matriz b pedir datos
+
 
 c = Array.new		#matriz c, contendra el resultado de la suma
 d = Array.new		#matriz d, contendra el resultado de la multiplicacion
 
-m,n = a.size,a[1].size
+#m,n = a.size,a[1].size 
+
+puts "Introduzca la dimension de la matriz cuadrada (m = n) : "
+m = gets.to_i	#gets.to_i hace que el valor obtenido se trate como entero
+n = m
+
+puts "Introduzca los valores de la matriz a"
+
+for i in 0...m do
+        a[i] = Array.new
+        for j in 0...n do
+		puts "Valor a[#{i}][#{j}]: "
+                a[i][j] = gets.to_i
+        end
+end 
+
+puts "Introduzca los valores de la matriz b"
+
+for i in 0...m do
+        b[i] = Array.new
+        for j in 0...n do
+                puts "Valor b[#{i}][#{j}]: "
+                b[i][j] = gets.to_i
+        end
+end 
 
 for i in 0...m do
 	c[i] = Array.new
@@ -29,15 +57,17 @@ for i in 0...m do
 	end
 end
 
+#la multiplicación da problemas 
+
 #hacer la multiplicacion
-
-e = Matrix[[3,5,6],[0,3,2],[5,5,4]] * Matrix[[1,1,1],[2,3,4],[6,5,9]]
-
-for i in 0...m do
-	for j in 0...n do
-		d[i][j] = e.component(i, j)
-	end
-end
+#
+#e = Matrix[[3,5,6],[0,3,2],[5,5,4]] * Matrix[[1,1,1],[2,3,4],[6,5,9]]
+#
+#for i in 0...m do
+#	for j in 0...n do
+#		d[i][j] = e.component(i, j)
+#	end
+#end
 
 #mostrar las matrices
 
